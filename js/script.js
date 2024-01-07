32e299240254e8b4e6cbbd4c65545c606da4e1d4
YouthIT999 <youthit999@gmail.com>
2024-01-07 16:10:36 +0545

    CEO portfolio

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*---------- Typing animation ----------*/
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "Web Designer", "Graphic Designer", "Youtuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+// /*---------- Aside ----------*/
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavlist = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+for (let i = 0; i < totalNavlist; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        // for (let j = 0; j < totalNavlist; j++) {
+        //     // if (navList[j].querySelector("a").classList.contains("active")) {
+        //     //     allSection[j].classList.add("back-section");
+        //     // }
+        //     navList[j].querySelector("a").classList.remove("active")
+        // }
+        this.classList.add("active")
+        showSection(this);
+    })
+
+}
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
