5e8d74e891824665d2fa8dbd2e001cde89e49bf0
wrohit55 <rohitdahal75@gmail.com>
2024-01-20 23:40:20 +0545

    Content improvised of about page

--- js/script.js
+++ js/script.js
@@ -32,7 +32,21 @@ function showSection(element) {
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
 }
+function updateNav(element) {
 
+    for (let i = 0; i < totalNavlist; i++) {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if (target === navList[i].querySelector("a").getAttribute("herf").split("#")[1]) {
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+document.querySelector(".hire-me").addEventListener("click", function () {
+
+    showSection(this);
+    updateNav(this);
+})
 const navTogglerBtn = document.querySelector(".nav-toggler"),
     aside = document.querySelector(".aside");
 navTogglerBtn.addEventListener("click", () => {
